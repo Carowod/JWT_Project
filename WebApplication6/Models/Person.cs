76de6bbe939200f0a2c51727022d71f5c20ea8a1
Razer <Razer@DESKTOP-47E1UUO>
2022-03-22 20:53:02 +0300

    Добавьте файлы проекта.

---
+++ WebApplication6/Models/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication6.Models
+{
+    public class Person
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+        public int Id { get; set; }
+        public string Token { get; set; }
+    }
+}
---
+++ WebApplication6/Startup.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace WebApplication6
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                    .AddJwtBearer(options =>
+                    {
+                        options.RequireHttpsMetadata = false;
+                        options.TokenValidationParameters = new TokenValidationParameters
+                        {
+                            ValidateIssuer = true,
+                            
+                            ValidIssuer = OptionsAuth.ISSUER,
+                            
+                            ValidateAudience = true,
+                            
+                            ValidAudience = OptionsAuth.AUDIENCE,
+                            
+                            ValidateLifetime = true,
+
+                            IssuerSigningKey = OptionsAuth.GetSymmetricSecurityKey(),
+                            ValidateIssuerSigningKey = true,
+                        };
+                    });
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseDeveloperExceptionPage();
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
